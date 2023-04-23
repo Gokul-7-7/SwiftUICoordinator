ff6ceea418046000986d505f1aa17e07decb9bf3
Gokul <gokul.b@mail.peoplehum.com>
2023-04-23 12:23:52 +0530

    Initial Commit

---
+++ SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by Gokul on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
